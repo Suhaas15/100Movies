5472f6c7908ed83857cb9cbc58024cca22db4b5c
Suhaas15 <suhaas15032001@gmail.com>
2025-09-01 15:26:49 -0700

    changes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ response = requests.get(URL)
 emp_webpage = response.text
 soup = BeautifulSoup(emp_webpage, "html.parser")
 
-movie_name = soup.find_all(name="h3", class_="title")
+movie_titles = soup.find_all(name="h3", class_="title")
 
 with open("movies.txt","w") as file:
-    for i in range(len(movie_name)-1,-1,-1):
-        file.write(f"{movie_name[i].get_text()}\n")
+    for i in range(len(movie_titles)-1,-1,-1):
+        file.write(f"{movie_titles[i].get_text()}\n")
